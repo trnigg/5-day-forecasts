2697cda771809d90e2fed5fdd436338c3402d73c
Tim Nigg <tim@MacBook-Pro.local>
2023-10-02 14:29:12 +0800

    Added function to dynamically render forecasts to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ let weatherContainer = document.querySelector('#weather-container');
 let searchHistoryContainer = document.querySelector('#search-history');
 let clearHistoryButton = document.querySelector('#clear-history-button');
 let currentWeatherContainer = document.querySelector('#current-weather-container');
-let forecastWeatherContainers = document.getElementsByClassName('forecast-weather-containers');
+let forecastWeatherContainers = document.getElementsByClassName('forecast-weather-container');
 
 // Going to use the deprecated built-in geocoding
 const API_KEY = '302392b3827a5512bab59a356ac0fa88';
@@ -48,6 +48,7 @@ function fetchCurrentWeather(city) {
 // Function to fetch forecast data from the API
 function fetchForecastWeather(city) {
     // Make a fetch request to the Weather API using the city
+    // NOTE: The better solution (which would simplify a lot of code) is hidden behind a paywall; https://api.openweathermap.org/data/2.5/forecast/***daily***?q=${city}&appid=${API_KEY}&units=metric
     let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`; // Use template-literals to insert 'city' parameter and 'API_KEY' constant and get temperature in celsius (metric).
     fetch(apiUrl) //initiates HTTP GET request
     .then(function (response) {
@@ -69,8 +70,8 @@ function fetchForecastWeather(city) {
     console.log(apiUrl);
 }
 
-  // Function to display weather data
-  function displayCurrentWeather(weatherData) {
+// Function to display weather data
+function displayCurrentWeather(weatherData) {
     // Clear the #current-weather-container
     currentWeatherContainer.innerHTML = "";
 
@@ -87,7 +88,7 @@ function fetchForecastWeather(city) {
     // Dependants 
     const windSpeedKmh = (windSpeed * 3.6).toFixed(2); // Converts windspeed from m/s to km/h and round to 2 decimal places
     const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}.png`;
-    const formattedTimestamp = dayjs.unix(dataTimestamp).format('lll');
+    const formattedTimestamp = dayjs.unix(dataTimestamp).format('ll');
 
 
     // Create and display the main weather card for today
@@ -104,7 +105,9 @@ function fetchForecastWeather(city) {
     const iconImageEl = document.createElement('img');
     iconImageEl.src = weatherIconUrl;
     iconImageEl.alt = weatherDescription;
-    
+
+
+    // TODO: REFACTOR THIS WHOLE SECTION TO GENERATE ONE CARD WITH THE BELOW ALL AS .innerHTML
     // Weather Details Container;
     const currentWeatherBodyEl = document.createElement("ul");
     currentWeatherBodyEl.classList.add("list-group", "list-group-flush");
@@ -134,26 +137,72 @@ function fetchForecastWeather(city) {
     // Add card to container
     currentWeatherContainer.appendChild(currentWeatherCard);
 
-
-
-
-    console.log(cityName);
-    console.log(countryCode);
-    console.log(temperature);
-    console.log(humidity);
-    console.log(windSpeed); // need to convert to km/h
-    console.log(weatherIconCode);
-    console.log(weatherDescription);
-    console.log(weatherIconUrl);
-    console.log(dataTimestamp);
     console.log(weatherData);
 
 }
   
-  // Function to display forecast data
-  function displayForecastWeather(forecastData) {
+// Function to display forecast data
+function displayForecastWeather(forecastData) {
 
+    console.log(forecastData);
+    // Filter out only the fetched data to only include 1 object per day (at noon)
+    // used: https://chat.openai.com/share/879bd8cf-c22c-4e1c-9142-0ecbf45872e6 as a starting point to achive this
+    // Defines the filtering criteria where the .dt_txt key contains a property of "12:00:00"
+    function filterForecastResults(item){
+        return item.dt_txt.endsWith("12:00:00");
+    }
+
+    // Uses the filter method to go through 'list' array and create a new array containing only the elements meeting the criteria defined in the related function above
+    const noonForecasts = forecastData.list.filter(filterForecastResults);
+
+    console.log(noonForecasts);
     // Create and display weather cards for the five-day forecast
+
+    // Iterate through and clear each forecast container
+    for (let i = 0; i < forecastWeatherContainers.length; i++) {
+        const eachContainer = forecastWeatherContainers[i];
+        eachContainer.innerHTML = "";
+        }
+
+    // Iterate through the filtered forecast data and create a card for each
+    for (let i = 0; i < noonForecasts.length && i < forecastWeatherContainers.length; i++) { //i must be less than number of forecast items and containers (should = 5)
+        const singleForecast = noonForecasts[i];
+        const singleForecastCard = document.createElement("div");
+        singleForecastCard.className = "card";
+
+        // Extract (raw) relevant data
+        const dataTimestamp = singleForecast.dt;
+
+
+        const temperature = singleForecast.main.temp;
+        const weatherIconCode = singleForecast.weather[0].icon;
+        const weatherDescription = singleForecast.weather[0].description;
+
+        const humidity = singleForecast.main.humidity;
+        const windSpeed = singleForecast.wind.speed; // In m/s by default
+
+        // Dependants / formatted data
+        const windSpeedKmh = (windSpeed * 3.6).toFixed(2); // Converts windspeed from m/s to km/h and round to 2 decimal places
+        const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}.png`;
+        const formattedTimestamp = dayjs.unix(dataTimestamp).format('ll');
+
+        // Create card content with formatted data and include bootstrap element classes for required formatting
+        singleForecastCard.innerHTML = `
+            <h4 class="card-header">${formattedTimestamp} <img src="${weatherIconUrl}" alt="${weatherDescription}"></h4>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <strong>Temperature:</strong>
+                    ${temperature}°C
+                </li>
+                <li class="list-group-item"><strong>Humidity:</strong> ${humidity}%</li>
+                <li class="list-group-item"><strong>Wind-Speed:</strong> ${windSpeedKmh}km/h</li>
+            </ul>
+        `;
+
+        // Append the forecast card to the corresponding forecast container
+        forecastWeatherContainers[i].appendChild(singleForecastCard);
+        }
+
 }
 
 
